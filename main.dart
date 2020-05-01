e583ea83cb3f2e02117e67164f212718ec041a7f
treeplate <tree.plate.moo@gmail.com>
2020-05-01 00:41:25 +0000

    delete comment

--- main.dart
+++ main.dart
@@ -1,17 +1,30 @@
 
-class OObject {
+class OObject{
   dynamic noSuchMethod(Invocation invocation){
     print(invocation.memberName);
-    return null;
+    if(invocation.isGetter) {
+      return map[invocation.memberName];
+    }
+    if(invocation.isSetter) {
+      map[Symbol(invocation.memberName.toString().substring(8, invocation.memberName.toString().length - 3))] = invocation.positionalArguments.first;
+    }
+    if(invocation.isMethod) {
+      Function.apply(map[invocation.memberName] as Function, invocation.positionalArguments);
+    }
+    return "???";
   }
+  String toString() => '$map';
+
+  Map<Symbol, dynamic> map = {};
 }
 
 void main(){
   dynamic oobject = OObject();
-  print(oobject.whatMethodIsThis());
+  oobject.cat = OObject();
+  oobject.cat.ada = OObject();
+  oobject.cat.ada.bob = OObject();
+  oobject.cat.ada.bob.addd = oobject.cat.ada.bob.nil;
+  print(oobject);
+  oobject.func = (String arg, List stringg){print(stringg + <String>[arg]);};
+  oobject.func("Hello!", <dynamic>["Hi, or"]);
 }
-
-/*stackedit_data:
-eyJoaXN0b3J5IjpbMTY3MDQzODk3NywxMDA3NjczOTAzLC0xMD
-kwNjcyODQ5XX0=
-*/
